8f4108bb944e020823784c3ca3bfb7ffbad92fcd
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2022-05-16 05:56:54 -0300

    A1. Parte 4.
    Criando dialog de autenticação.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
+import 'package:bytebank/components/transaction_auth_dialog.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 //Todo implementar update e delete e depois ir para o kart manager.
 void main() {
   runApp(BytebankApp());
-  //save(Transaction(200.0, Contact(0, 'Gui', 2000))).then((transaction) => print(transaction));
-  //findAll().then((transactions) => print('new transactions $transactions'));
 }
 
 class BytebankApp extends StatelessWidget {
@@ -20,7 +19,7 @@ class BytebankApp extends StatelessWidget {
           textTheme: ButtonTextTheme.primary,
         ),
       ),
-      home: Dashboard(),
+      home: TransactionAuthDialog(),
     );
   }
 }
