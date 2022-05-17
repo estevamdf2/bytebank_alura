8013690bdd2b7dee0cdb427d7d64a8c33e3270ef
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2022-05-17 05:52:15 -0300

    A1 parte 8. Transferindo com autenticação.

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/components/transaction_auth_dialog.dart';
 import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/http/webclients/transaction_webclient.dart';
 import 'package:bytebank/models/contact.dart';
@@ -65,11 +66,22 @@ class _TransactionFormState extends State<TransactionForm> {
                           double.tryParse(_valueController.text);
                       final transactionCreated =
                           Transaction(value, widget.contact);
-                      _webClient.save(transactionCreated).then((transaction) {
-                        if (transaction != null) {
-                          Navigator.pop(context);
-                        }
-                      });
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return TransactionAuthDialog(
+                              onConfirm: (String password) {
+                                print(password);
+                                _webClient
+                                    .save(transactionCreated, password)
+                                    .then((transaction) {
+                                  if (transaction != null) {
+                                    Navigator.pop(context);
+                                  }
+                                });
+                              },
+                            );
+                          });
                     },
                   ),
                 ),
